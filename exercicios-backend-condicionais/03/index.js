168388f487e7adfe2bdd7cd22077ee1259344b6f
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-18 20:41:56 -0300

    feito exercício dominó

--- exercicios-backend-condicionais/03/index.js
+++ exercicios-backend-condicionais/03/index.js
@@ -1,4 +1,9 @@
-const ladoA = 3;
-const ladoB = 3;
+const ladoA = 6;
+const ladoB = 6;
 
-//seu código aqui
+const dominoBucha = ladoA === ladoB ? "SIM" : "NÃO";
+const validacao = ladoA >= 1 && ladoA <= 6 && ladoB >= 1 && ladoB <= 6;
+
+if (validacao) {
+  console.log(`A peça que você pegou é uma bucha? ${dominoBucha}`);
+} else console.log("Digite uma peça válida");
