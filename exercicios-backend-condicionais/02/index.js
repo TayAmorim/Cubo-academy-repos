8bf43ee94b18ed21741b135481069eceac469a24
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-18 20:27:36 -0300

    exercício par ou ímpar concluído

--- exercicios-backend-condicionais/02/index.js
+++ exercicios-backend-condicionais/02/index.js
@@ -1,4 +1,15 @@
-const jogada1 = 5;
-const jogada2 = 3;
+const jogada1 = 1;
+const jogada2 = 2;
 
-//seu código aqui
+const soma = jogada1 + jogada2;
+const par = soma % 2 == 0;
+
+if (jogada1 >= 1 && jogada2 >= 1) {
+  if (par) {
+    console.log("O ganhador foi aquele que escolheu Par");
+  } else {
+    console.log("O ganhador foi aquele que escolheu ímpar");
+  }
+} else {
+  console.log("Digite um número válido");
+}
