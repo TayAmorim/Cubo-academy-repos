3022c94c70f4ce8e5751ca400b54135b41c0163f
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 11:59:35 -0300

    realizando o exercicio 11

--- exercicios-backend-condicionais/11/index.js
+++ exercicios-backend-condicionais/11/index.js
@@ -1,8 +1,14 @@
-//renda mensal do aluno, em centavos.
 const rendaMensalEmCentavos = 300000;
-
-// Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
-const mesesDecorridos = 12;
-
-// Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
+const mesesDecorridos = 60;
 const totalJaPagoPeloAluno = 1000000;
+
+const naoPagaNada = mesesDecorridos > 60 || totalJaPagoPeloAluno === 1800000;
+const inicioDoPagamento = rendaMensalEmCentavos > 2000000;
+const valorParcelaTaxa = (
+  ((18 / 100) * rendaMensalEmCentavos) /
+  100
+).toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+console.log(valorParcelaTaxa);
+
+if (inicioDoPagamento) {
+}
