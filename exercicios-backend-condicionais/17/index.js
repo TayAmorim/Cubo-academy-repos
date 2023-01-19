832394e81ee89f4bca6a349c9d790d1b27f60c2c
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 16:16:35 -0300

    finalizando a lista de exercicio de condicionais

--- exercicios-backend-condicionais/17/index.js
+++ exercicios-backend-condicionais/17/index.js
@@ -1,8 +1,20 @@
-//valor do produto comprado.
-const valorDoProduto = 100000;
-
-//quantidade de parcelas
-const quantidadeDoParcelamento = 10;
-
-//valor pago
+const valorDoProduto = 100000 / 100;
+const quantidadeDoParcelamento = 12;
 const valorPago = 300;
+
+const valorParcela = valorDoProduto / quantidadeDoParcelamento;
+const qtDeParcelasParaPagar = Math.abs(
+  Math.round(valorPago / valorParcela) - quantidadeDoParcelamento
+);
+
+if (quantidadeDoParcelamento > qtDeParcelasParaPagar) {
+  console.log(
+    `Restam ${qtDeParcelasParaPagar} parcelas de ${valorParcela.toLocaleString(
+      "pt-BR",
+      {
+        style: "currency",
+        currency: "BRL",
+      }
+    )}`
+  );
+}
