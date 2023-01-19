a9b6868800354123dfa3bb9c6a1e620ef8d43fed
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 11:34:55 -0300

    identificando caracteres - concluído

--- exercicios-backend-condicionais/10/index.js
+++ exercicios-backend-condicionais/10/index.js
@@ -1,1 +1,31 @@
 const caractere = "E";
+
+const vogalMaiuscula =
+  caractere === "A" ||
+  caractere === "E" ||
+  caractere === "I" ||
+  caractere === "O" ||
+  caractere === "U";
+
+const vogalMinuscula =
+  caractere === "a" ||
+  caractere === "e" ||
+  caractere === "i" ||
+  caractere === "o" ||
+  caractere === "u";
+
+const vogal = vogalMaiuscula || vogalMinuscula;
+const numero = isNaN(caractere);
+const consoante = !vogal && numero;
+
+if (caractere) {
+  if (vogal) {
+    if (vogalMaiuscula) {
+      console.log("Vogal maiúscula");
+    } else console.log("Vogal minúscula");
+  } else if (!numero) {
+    console.log("Número");
+  } else if (consoante) {
+    console.log("Consoante");
+  }
+} else console.log("Caractere vazio");
