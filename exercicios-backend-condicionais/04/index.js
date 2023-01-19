1084a0a69bc3dfc7004ce20c262d3f15c9bfeb7f
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-18 21:06:47 -0300

    Exercício dano nome as pedras concluído

--- exercicios-backend-condicionais/04/index.js
+++ exercicios-backend-condicionais/04/index.js
@@ -1,11 +1,34 @@
 const ladoA = 6;
 const ladoB = 6;
 
-const dominoBucha = ladoA === ladoB ? "SIM" : "NÃO";
+const bucha = ladoA === ladoB;
 const validacao = ladoA >= 0 && ladoA <= 6 && ladoB >= 0 && ladoB <= 6;
 
+const branco = !ladoA && ladoB < 1;
+const as = ladoA && ladoB === 1;
+const duque = ladoA && ladoB === 2;
+const terno = ladoA && ladoB === 3;
+const quadra = ladoA && ladoB === 4;
+const quina = ladoA && ladoB === 5;
+const sena = ladoA && ladoB === 6;
+
 if (validacao) {
-  console.log(`A peça que você pegou é uma bucha? ${dominoBucha}`);
-  if (ladoA === ladoB) {
-  }
+  if (bucha) {
+    console.log("A peça que você pegou é uma bucha");
+    if (branco) {
+      console.log("Bucha de Branco");
+    } else if (as) {
+      console.log("Bucha de Ás");
+    } else if (duque) {
+      console.log("Bucha de Duque");
+    } else if (terno) {
+      console.log("Bucha de Terno");
+    } else if (quadra) {
+      console.log("Bucha de Quadra");
+    } else if (quina) {
+      console.log("Bucha de Quina");
+    } else if (sena) {
+      console.log("Bucha de Sena");
+    }
+  } else console.log("A peça que você pegou não é uma bucha");
 } else console.log("Digite uma peça válida");
