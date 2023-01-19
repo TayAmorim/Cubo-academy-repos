81e52021396c3170cbbd2a874711cb43e68f2f5e
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 10:01:22 -0300

    exercício 06 concluído

--- exercicios-backend-condicionais/06/index.js
+++ exercicios-backend-condicionais/06/index.js
@@ -1,3 +1,22 @@
-const alturaEmCm = 185;
+const alturaEmCm = 152;
+const filtro = 180;
+const peneiraVolei = alturaEmCm >= filtro;
 
-//seu código aqui
+const libero = alturaEmCm >= 180 && alturaEmCm <= 185;
+const ponteiro = alturaEmCm >= 186 && alturaEmCm <= 195;
+const oposto = alturaEmCm >= 196 && alturaEmCm < 205;
+const central = alturaEmCm >= 205;
+
+if (peneiraVolei) {
+  if (libero) {
+    console.log("Sua posição é LÍBERO");
+  } else if (ponteiro) {
+    console.log("Sua posição é PONTEIRO");
+  } else if (oposto) {
+    console.log("Sua posição é OPOSTO");
+  } else {
+    console.log("Sua posição é CENTRAL");
+  }
+} else {
+  console.log("Você foi reprovado");
+}
