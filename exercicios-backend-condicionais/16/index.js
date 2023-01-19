310774c1f34f32dd2b7c1dd0543eb8b65749ffd6
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 15:42:29 -0300

    exercício rematricula escolar finalizado

--- exercicios-backend-condicionais/16/index.js
+++ exercicios-backend-condicionais/16/index.js
@@ -1,5 +1,12 @@
-//Só é considerado de maior se a idade for maior ou igual a 18.
-const idadeDoAluno = 16;
-
-//valor verdadeiro ou falso que informa se possui responsável
+const idadeDoAluno = 18;
 const possuiResponsavel = false;
+
+const precisaResponsavel = idadeDoAluno >= 18;
+
+if (precisaResponsavel) {
+  console.log("Rematrícula realizada com sucesso");
+} else if (!precisaResponsavel && possuiResponsavel) {
+  if (possuiResponsavel) {
+    console.log("Rematrícula realizada com sucesso");
+  }
+} else console.log("Não é possível fazer a rematrícula");
