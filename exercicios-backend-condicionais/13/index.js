3534f7c73df9c4ae572c054cd481144123eba1f0
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 14:04:13 -0300

    exercicio 123 finalizado

--- exercicios-backend-condicionais/13/index.js
+++ exercicios-backend-condicionais/13/index.js
@@ -1,6 +1,28 @@
-//tipo de pagamento (dinheiro, credito, debito, cheque).
 const tipoDePagamento = "credito";
+const valorDoProduto = 13000 / 100;
 
-//valor da mercadoria (centavos)
-const valorDoProduto = 13000;
-
+if (tipoDePagamento === "credito") {
+  const desconto = 5 / 100;
+  const valorFinal = valorDoProduto - valorDoProduto * desconto;
+  console.log(
+    `Você escolheu ${tipoDePagamento}, Então o  valor a ser pago: R$ ${valorFinal.toFixed(
+      2
+    )}`
+  );
+} else if (tipoDePagamento === "cheque") {
+  const desconto = 3 / 100;
+  const valorFinal = valorDoProduto - valorDoProduto * desconto;
+  console.log(
+    `Você escolheu ${tipoDePagamento}, Então o  valor a ser pago: R$ ${valorFinal.toFixed(
+      2
+    )}`
+  );
+} else if (tipoDePagamento === "debito" || tipoDePagamento === "dinheiro") {
+  const desconto = 10 / 100;
+  const valorFinal = valorDoProduto - valorDoProduto * desconto;
+  console.log(
+    `Você escolheu ${tipoDePagamento}, Então o valor a ser pago: R$ ${valorFinal.toFixed(
+      2
+    )}`
+  );
+}
