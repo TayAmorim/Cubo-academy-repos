156d6dee175d0dcd126be80d773a68e816ee100c
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 14:12:50 -0300

    finalizando exercicio 14: Bebam água

--- exercicios-backend-condicionais/14/index.js
+++ exercicios-backend-condicionais/14/index.js
@@ -1,3 +1,18 @@
-//quantidade de água ingerida em litro.
-const quantidadeDeAguaIngerida = 2;
+const quantidadeDeAguaIngerida = 1;
 
+const riscoAlto = quantidadeDeAguaIngerida < 1.5;
+const riscoModerado =
+  quantidadeDeAguaIngerida > 1.5 && quantidadeDeAguaIngerida <= 3;
+const riscoBaixo = quantidadeDeAguaIngerida > 3;
+
+if (riscoAlto) {
+  console.log(
+    "Risco Alto - Você está ingerindo pouquissima água, beba mais água!"
+  );
+} else if (riscoModerado) {
+  console.log("Risco Moderado - Você está ingerindo pouca água, beba mais!");
+} else {
+  console.log(
+    "Risco Baixo - Você está ingerindo uma boa quantidade de água, parabéns!"
+  );
+}
