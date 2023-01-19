e4b4d9a90499f5e292416fc98e3547aefed3c4ce
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 11:02:54 -0300

    exercício 9 concluído

--- exercicios-backend-condicionais/09/index.js
+++ exercicios-backend-condicionais/09/index.js
@@ -1,1 +1,22 @@
-const nota = 8.5;
+const nota = 3;
+
+const validacao = nota >= 0 && nota <= 10;
+
+const conceitoA = nota >= 9;
+const conceitoB = nota >= 8 && nota <= 8.9;
+const conceitoC = nota >= 6 && nota <= 7.9;
+const conceitoD = nota >= 4 && nota <= 5.9;
+
+if (validacao) {
+  if (conceitoA) {
+    console.log("O estudande obteve conceito A");
+  } else if (conceitoB) {
+    console.log("O estudande obteve conceito B");
+  } else if (conceitoC) {
+    console.log("O estudande obteve conceito C");
+  } else if (conceitoD) {
+    console.log("O estudande obteve conceito D");
+  } else console.log("O estudando obteve conceito E");
+} else {
+  console.log("Nota invalida");
+}
