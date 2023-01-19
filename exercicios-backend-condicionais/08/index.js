5812809bec2a88512d0dced0379865f501b36c9c
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 10:50:01 -0300

    exercício montanha russa finalizada

--- exercicios-backend-condicionais/08/index.js
+++ exercicios-backend-condicionais/08/index.js
@@ -1,5 +1,17 @@
-const idade = 18;
+const idade = 17;
 const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+const acessoNegado =
+  idade < 12 || idade > 65 || possuiPatologia || altura < 150;
+
+const ingresso10 = idade < 18 || ehEstudante;
+
+if (acessoNegado) {
+  console.log("ACESSO NEGADO");
+} else if (ingresso10) {
+  console.log("O valor do seu ingresso é 10 reais para poder brincar");
+} else {
+  console.log("O valor do seu ingresso é 20 reais para poder brincar");
+}
