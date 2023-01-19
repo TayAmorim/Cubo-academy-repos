18967d2afe617fd4f5fd9167a740426ac7dabff9
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 13:35:50 -0300

    Exercício Dia da Semana

--- exercicios-backend-condicionais/12/index.js
+++ exercicios-backend-condicionais/12/index.js
@@ -1,1 +1,23 @@
-const diaDaSemana = 3;
+const diaDaSemana = 8;
+
+const validacao = diaDaSemana < 1 || diaDaSemana > 7;
+
+if (!validacao) {
+  if (diaDaSemana === 1) {
+    console.log("Segunda-Feira");
+  } else if (diaDaSemana === 2) {
+    console.log("Terça-Feira");
+  } else if (diaDaSemana === 3) {
+    console.log("Quarta-Feira");
+  } else if (diaDaSemana === 4) {
+    console.log("Quinta-Feira");
+  } else if (diaDaSemana === 5) {
+    console.log("Sexta-Feira");
+  } else if (diaDaSemana === 6) {
+    console.log("Sábado");
+  } else {
+    console.log("Domingo");
+  }
+} else {
+  console.log("O dia da semana informado não é válido.");
+}
