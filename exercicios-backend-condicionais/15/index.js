c4819301f25df57ffdae6b1bfdf99c8097a12f49
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 15:31:01 -0300

    finalizando exercício 15

--- exercicios-backend-condicionais/15/index.js
+++ exercicios-backend-condicionais/15/index.js
@@ -1,4 +1,15 @@
 const primeiroNome = "Mario";
 const sobrenome = "";
-const apelido = "";
+const apelido = "Bros";
 
+if (primeiroNome) {
+  if (primeiroNome && !sobrenome && !apelido) {
+    console.log(`${primeiroNome}`);
+  } else if (primeiroNome && apelido && !sobrenome) {
+    console.log(`${primeiroNome} ${apelido}`);
+  } else {
+    console.log(`${primeiroNome} ${sobrenome}`);
+  }
+} else {
+  console.log("É necesessário que você digite um nome");
+}
