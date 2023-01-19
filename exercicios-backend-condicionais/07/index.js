2b2f6b00af5d7c68e8ce28d4b380784d9fff1a28
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-19 10:37:21 -0300

    exercício 06 concluído

--- exercicios-backend-condicionais/07/index.js
+++ exercicios-backend-condicionais/07/index.js
@@ -1,5 +1,16 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 1500000; //emCentavos
+const rendimentoMinimo = 2855970;
 
-//seu código aqui
+const isenta = aposentada || portadoraDeDoenca;
+const pegaLeao = !isenta && totalDeRendimentos > rendimentoMinimo;
+const semRendimentoM = totalDeRendimentos < rendimentoMinimo;
+
+if (isenta) {
+  console.log("ISENTA");
+} else if (pegaLeao) {
+  console.log("PEGA LEAO");
+} else if (semRendimentoM) {
+  console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
+}
