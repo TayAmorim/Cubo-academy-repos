75e4a7abd71eb61eb916247d59bf1bea6a1f328e
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-20 20:28:24 -0300

    finalizando exercicio 11

--- arrays-loops/11/index.js
+++ arrays-loops/11/index.js
@@ -1,2 +1,11 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const original = [5, 7, 10, 13, 17, 21, 26, 34, 100, 118, 245];
 
+const novoArray = [];
+
+for (let numero of original) {
+  if ((numero >= 10 && numero <= 20) || numero > 100) {
+    novoArray.push(numero);
+  }
+}
+
+console.log(novoArray);
