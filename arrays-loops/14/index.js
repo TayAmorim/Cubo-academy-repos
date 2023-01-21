ea2b4956963d73880778c0a50a39eec341b792b4
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-20 22:24:47 -0300

    finalizando exercicio 14

--- arrays-loops/14/index.js
+++ arrays-loops/14/index.js
@@ -1,3 +1,12 @@
-const arrayA = [5, 32, 3, 44, 1];
-const arrayB = [57, 4, 21, 2, 13];
+const arrayA = [1, 3, 3, 44, 11];
+const arrayB = [57, 4, 21, 32, 13];
 
+for (let i = 0; i < arrayA.length; i++) {
+  const a = arrayA[i];
+  const b = arrayB[i];
+  if (a < b) {
+    console.log(a);
+  } else if (b < a) {
+    console.log(b);
+  }
+}
