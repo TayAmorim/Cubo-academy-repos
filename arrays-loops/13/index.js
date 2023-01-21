b859030c8ef409ecb4e72ffb1b982bc4a1c569de
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-20 21:41:28 -0300

    Exercício Verificando disjuntores concluído

--- arrays-loops/13/index.js
+++ arrays-loops/13/index.js
@@ -1,2 +1,18 @@
-const disjuntores = [false, false, true, false, false, true, false, false];
+const disjuntores = [
+  false,
+  true,
+  true,
+  false,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
 
+for (let i = 0; i < disjuntores.length; i++) {
+  if (disjuntores[i]) {
+    console.log(i);
+  }
+}
