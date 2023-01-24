78a533bc05566b0e9bab7b7e417ce425d69cb319
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-24 18:43:29 -0300

    adicionando o repositorio objetos

---
+++ objetos/01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+  rua: "Dona julieta",
+  numero: 81,
+  complemento: "Apartamento",
+  bairro: "Encruzilhada",
+  cep: 52041550,
+  cidade: "Recife",
+  estado: "pe",
+  pais: "Brasil",
+};
+
+const usuario = {
+  nome: "Tayanna",
+  email: "Tay.kelly@hotmail.com",
+  telefone: 85774220,
+  data_nascimento: 26_04_1995,
+  endereco,
+};
+
+console.log(usuario);
