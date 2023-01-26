3f228e96674b6c7c04df5989ec9fae0d14482e11
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-26 08:31:02 -0300

    adicionando o repositório funções

---
+++ funcoes/01/index.js
@@ -0,0 +1,28 @@
+const prova = {
+    aluno: "João",
+    materia: "Português",
+    valor: 10,
+    questoes: [
+        {
+            resposta: "a",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        },
+        {
+            resposta: "e",
+            correta: "b"
+        },
+        {
+            resposta: "b",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        }
+    ]
+};
+
---
+++ funcoes/03/index.js
@@ -0,0 +1,17 @@
+const carrinho = {
+    nomeDoCliente: "Guido Bernal",
+    produtos: [
+        {
+            id: 1,
+            nome: "Camisa",
+            qtd: 3,
+            precoUnit: 3000
+        },
+        {
+            id: 2,
+            nome: "Bermuda",
+            qtd: 2,
+            precoUnit: 5000
+        }
+    ]
+}
