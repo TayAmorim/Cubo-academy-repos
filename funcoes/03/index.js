314c7301547907b4f24ee51ff50e17ba9800ba83
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-26 11:05:07 -0300

    parte 2 finalizada

--- funcoes/03/index.js
+++ funcoes/03/index.js
@@ -14,19 +14,61 @@ const carrinho = {
       precoUnit: 5000,
     },
   ],
+  imprimirResumo: function () {
+    let totalItens = 0;
+    let totalPagar = 0;
+    for (let qtdItem of this.produtos) {
+      totalItens += qtdItem.qtd;
+      totalPagar += (qtdItem.qtd * qtdItem.precoUnit) / 100;
+    }
+    return `
+    Cliente: ${this.nomeDoCliente}
+    Total de Itens: ${totalItens} itens
+    Total a pagar: R$ ${totalPagar.toFixed(2)}`;
+  },
 };
 
-function imprimirResumoDoCarrinho(carrinho) {
-  let totalItens = 0;
-  let totalPagar = 0;
-  for (let qtdItem of carrinho.produtos) {
-    totalItens += qtdItem.qtd;
-    totalPagar += (qtdItem.qtd * qtdItem.precoUnit) / 100;
+const novoTenis = {
+  id: 3,
+  nome: "Tenis",
+  qtd: 1,
+  precoUnit: 10000,
+};
+const novaBermuda = {
+  id: 2,
+  nome: "Bermuda",
+  qtd: 3,
+  precoUnit: 5000,
+};
+
+function addProdutoAoCarrinho(carrinho, produto) {
+  for (let i = 0; i < carrinho.produtos.length; i++) {
+    if (carrinho.produtos[i].id === produto.id) {
+      carrinho.produtos[i].qtd += produto.qtd;
+    }
+    if (
+      carrinho.produtos[i].id !== produto.id &&
+      carrinho.produtos[i + 1].id !== produto.id
+    ) {
+      carrinho.produtos.push(produto);
+      break;
+    }
   }
-  return `
-  Cliente: ${carrinho.nomeDoCliente}
-  Total de Itens: ${totalItens} itens
-  Total a pagar: R$ ${totalPagar.toFixed(2)}`;
 }
 
-console.log(imprimirResumoDoCarrinho(carrinho));
+/* function addProdutoAoCarrinho(carrinho, produto) {
+  for (let item of carrinho.produtos) {
+    if (item.id === produto.id) {
+      item.qtd += produto.qtd;
+      console.log(item.id);
+    }
+    if (item.id !== produto.id) {
+      carrinho.produtos.push(produto);
+      break;
+    }
+  }
+} */
+addProdutoAoCarrinho(carrinho, novaBermuda);
+addProdutoAoCarrinho(carrinho, novoTenis);
+console.log(carrinho.imprimirResumo());
+console.log(carrinho.produtos);
