76270b2f6dcb98ff4c85530eb18b08787ee58f83
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-01-27 20:07:43 -0300

    finalizando a lista de exercicio metodo String

--- metodos-strings/10/index.js
+++ metodos-strings/10/index.js
@@ -1,1 +1,19 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = "Foto da Familia.hvg";
+const arquivosAceitos = ["jpg", "jpeg", "gif", "png"];
+
+function validacaoArquivos(nomeArquivo, arquivosAceitos) {
+  let mensagem = "";
+  let validar = [];
+  for (let arquivos of arquivosAceitos) {
+    if (nomeArquivo.includes(arquivos)) {
+      validar.push(arquivos);
+    }
+    if (validar.length === 1) {
+      mensagem = "Arquivo Válido";
+    } else {
+      mensagem = "Arquivo Invalido";
+    }
+  }
+  return mensagem;
+}
+console.log(validacaoArquivos(nomeArquivo, arquivosAceitos));
