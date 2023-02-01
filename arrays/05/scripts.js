5dea474204732fadcef7f6f4b7d6906ee4c98a4d
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-02-01 15:41:51 -0300

    finalizando exercicio 05

--- arrays/05/scripts.js
+++ arrays/05/scripts.js
@@ -1,1 +1,22 @@
-const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+const pacientes = ["José", "Pedro", "Maria", "João", "Ana", "Bárbara", "Joana"];
+
+function agendarPaciente(pacientes, nome) {
+  pacientes.splice(0, 1);
+  pacientes.splice(pacientes.length, 0, nome);
+  console.log(pacientes.join());
+}
+
+function atenderPaciente(pacientes) {
+  pacientes.splice(0, 1);
+  console.log(pacientes.join());
+}
+
+function cancelarAtendimento(pacientes, nome) {
+  pacientes.splice(nome, 1);
+  console.log(pacientes.join());
+}
+
+agendarPaciente(pacientes, "José");
+atenderPaciente(pacientes);
+cancelarAtendimento(pacientes, "Maria");
+atenderPaciente(pacientes);
