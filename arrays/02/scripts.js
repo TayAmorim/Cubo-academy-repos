abbccb01a422bad39da79ee48ecd48f1547374e8
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-02-01 21:25:44 -0300

    finalizando llista array

--- arrays/02/scripts.js
+++ arrays/02/scripts.js
@@ -1,20 +1,15 @@
 const nomes = ["Juninho", "Vidal", "Guido", "Dani", "Ruli", "Diego"];
-const tamanhoDoGrupo = 3;
+const tamanhoDoGrupo = 4;
 
 function divisaoGrupos(nomes, tamanhoDoGrupo) {
-  const grupo = [];
-  const grupos = nomes.length / tamanhoDoGrupo;
-  if (grupos % 2 === 0) {
-    for (let i = 0; i < grupos; i++) {
-      if (i === 0) {
-        grupo.push(nomes.slice(0, tamanhoDoGrupo));
-      } else {
-        console.log(i * tamanhoDoGrupo, grupos * i * tamanhoDoGrupo);
-        grupo.push(nomes.slice(i * tamanhoDoGrupo, grupos * i));
-      }
-    }
-    console.log(grupo);
+  const grupos = [];
+  const quatantidadeGrupos = nomes.length / tamanhoDoGrupo;
+
+  for (let i = 0; i < quatantidadeGrupos; i++) {
+    const result = nomes.splice(0, tamanhoDoGrupo);
+    grupos.push(result);
   }
+  console.log(grupos);
 }
 
 divisaoGrupos(nomes, tamanhoDoGrupo);
