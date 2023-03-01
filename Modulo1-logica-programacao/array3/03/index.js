1ed45157a4773b64133b00fe0d846621be918c92
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-03-01 15:27:22 -0300

    Organizando melhor os assuntos

--- Modulo1-logica-programacao/array3/03/index.js
+++ Modulo1-logica-programacao/array3/03/index.js
@@ -1,4 +1,4 @@
-const numeros = [1, 98, -76, 0, 122, 19, 5, 60, 44];
+/* const numeros = [1, 98, -76, 0, 122, 19, 5, 60, 44];
 
 const maiorNumero = numeros.reduce((acumulador, valorAtual) => {
   if (acumulador > valorAtual) {
@@ -8,3 +8,9 @@ const maiorNumero = numeros.reduce((acumulador, valorAtual) => {
 });
 
 console.log(maiorNumero);
+ */
+
+const frase = "Seja feliz";
+const resumo = frase.split(" ");
+
+console.log(resumo);
