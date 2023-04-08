849ed7a5f75caa934e8428e2b280c48185d9c79c
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-04-08 10:19:26 -0300

    faltar finalizar

---
+++ Modulo3-Dom/manipulando-dom/exercicio6/script.js
@@ -0,0 +1,16 @@
+const avatar = document.querySelector("img");
+const imgs = [2, 3, 4, 5, 6, 7, 8];
+const body = document.querySelector("body");
+
+const atualwidth = body.offsetWidth / 9;
+let incrementalWidth = 0;
+const teste = avatar.offsetWidth;
+
+for (let i = 0; i < imgs.length; i++) {
+  setTimeout(() => {
+    avatar.src = `assets/${imgs[i]}.png`;
+    avatar.style.left = `${incrementalWidth}px`;
+    incrementalWidth += atualwidth;
+    console.log(incrementalWidth, atualwidth);
+  }, 600 * i);
+}
