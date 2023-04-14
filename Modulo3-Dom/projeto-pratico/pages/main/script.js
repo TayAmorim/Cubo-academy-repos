6c78d5ef3803c07f50463a1fb1eea7fb76a8ab64
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-04-14 14:30:46 -0300

    finalizando o projeto

--- Modulo3-Dom/projeto-pratico/pages/main/script.js
+++ Modulo3-Dom/projeto-pratico/pages/main/script.js
@@ -1,30 +1,58 @@
 const logout = document.querySelector(".icon-logout");
+
 const form = document.querySelector(".form-modal-contact");
 const inputs = document.querySelectorAll(".form-modal-contact input");
 const modalContact = document.querySelector(".modal-contact");
 const btnOpenModalContact = document.querySelector(".btn-contact");
 const btnAdicionarNewContact = document.querySelector(".btn-add");
 const closedModalContact = document.querySelector(".closed-modal-contact");
-const btnCancel = document.querySelector(".btn-cancel");
+const btnCancel = document.querySelector(".btn-cancel-contact");
+
+const btnOpenModalExclusion = document.querySelectorAll(".delet-contact");
+const modalExclusion = document.querySelector(".modal-exclusion");
+const btnExclusionModal = document.querySelector(".btn-exclusion-modal");
+const btnCancelModalExclusion = document.querySelector(".btn-cancel-exclusion");
+const btnclosedModalExclusion = document.querySelector(
+  ".closed-modal-exclusion"
+);
+
+btnOpenModalExclusion.forEach((btn) => {
+  btn.addEventListener("click", openModalExclusion);
+});
 
 btnOpenModalContact.addEventListener("click", openModalNewContact);
-closedModalContact.addEventListener("click", closedModal);
-btnCancel.addEventListener("click", closedModal);
 
-function openModalNewContact() {
-  modalContact.classList.remove("disabled");
-}
+[btnExclusionModal, btnCancelModalExclusion, btnclosedModalExclusion].forEach(
+  (elemento) => {
+    elemento.addEventListener("click", closedModalExclusion);
+  }
+);
 
-function closedModal() {
-  modalContact.classList.add("disabled");
-}
+[closedModalContact, btnCancel].forEach((elemento) => {
+  elemento.addEventListener("click", closeModalNewContact);
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  closedModal();
+  closeModalNewContact();
 });
 
 logout.addEventListener("click", () => {
   location = "../../index.html";
 });
+
+function openModalNewContact() {
+  modalContact.classList.remove("disabled");
+}
+
+function closeModalNewContact() {
+  modalContact.classList.add("disabled");
+}
+
+function openModalExclusion() {
+  modalExclusion.classList.remove("disabled");
+}
+
+function closedModalExclusion() {
+  modalExclusion.classList.add("disabled");
+}
