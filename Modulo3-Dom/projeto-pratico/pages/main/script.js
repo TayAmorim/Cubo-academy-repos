2ce2b97c28a97630c72448940e9dc9846b5ffabc
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-04-14 13:51:07 -0300

    modal novo contato parte javascript finalizado

--- Modulo3-Dom/projeto-pratico/pages/main/script.js
+++ Modulo3-Dom/projeto-pratico/pages/main/script.js
@@ -1,13 +1,30 @@
+const logout = document.querySelector(".icon-logout");
+const form = document.querySelector(".form-modal-contact");
+const inputs = document.querySelectorAll(".form-modal-contact input");
 const modalContact = document.querySelector(".modal-contact");
 const btnOpenModalContact = document.querySelector(".btn-contact");
+const btnAdicionarNewContact = document.querySelector(".btn-add");
 const closedModalContact = document.querySelector(".closed-modal-contact");
+const btnCancel = document.querySelector(".btn-cancel");
 
 btnOpenModalContact.addEventListener("click", openModalNewContact);
+closedModalContact.addEventListener("click", closedModal);
+btnCancel.addEventListener("click", closedModal);
 
 function openModalNewContact() {
   modalContact.classList.remove("disabled");
 }
 
-closedModalContact.addEventListener("click", () => {
+function closedModal() {
   modalContact.classList.add("disabled");
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  closedModal();
+});
+
+logout.addEventListener("click", () => {
+  location = "../../index.html";
 });
