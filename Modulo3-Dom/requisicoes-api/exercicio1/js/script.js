6bace18dfd3e234635e0478ec9f57b9b97df1c1f
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-04-20 17:00:24 -0300

    refatorando o código

--- Modulo3-Dom/requisicoes-api/exercicio1/js/script.js
+++ Modulo3-Dom/requisicoes-api/exercicio1/js/script.js
@@ -1,13 +1,16 @@
-const cards = document.querySelectorAll(".card");
+const body = document.querySelector("body");
 
 async function users() {
   const response = await api.get("/users");
-
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].children[0].innerText = response.data[i].name;
-    cards[i].children[1].innerText = response.data[i].email;
-    cards[i].children[2].innerText = response.data[i].phone;
-  }
+  const dataResponse = response.data;
+  dataResponse.forEach((user) => {
+    body.innerHTML += `
+  <div class="card">
+  <h1>${user.name}</h1>
+  <span>${user.email}</span>
+  <strong>${user.phone}</strong>
+  </div>`;
+  });
 }
 
 users();
