a4c8fdfb133e91e01ecbdd1beecfa2a045983512
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-04-25 09:18:38 -0300

    verificação se o input está vazio

--- Modulo3-Dom/criando-elementos/exercicio1/js/script.js
+++ Modulo3-Dom/criando-elementos/exercicio1/js/script.js
@@ -1,6 +1,7 @@
 const novaTarefa = document.querySelector(".nova-tarefa");
 const btnSucess = document.querySelector(".btn-success");
 const main = document.querySelector(".conteudo-principal");
+const containerNewTask = document.querySelector(".container-new-task");
 const img = document.querySelector("img");
 
 btnSucess.addEventListener("click", adicionarTarefa);
@@ -15,16 +16,20 @@ function criarElemento(tarefa) {
   const divTask = document.createElement("div");
   const span = document.createElement("span");
   const img = document.createElement("img");
-  main.appendChild(divContainer);
-  divContainer.appendChild(divTask);
-  divTask.appendChild(span);
-  divContainer.classList.add("container-tasks");
-  divTask.classList.add("task");
-  span.innerText = `${tarefa}`;
-  divTask.appendChild(img);
-  img.src = "assets/remove.svg";
-
-  img.addEventListener("click", () => {
-    main.removeChild(divContainer);
-  });
+  novaTarefa.style.border = "1px solid black";
+  if (tarefa !== "") {
+    main.appendChild(divContainer);
+    divContainer.appendChild(divTask);
+    divTask.appendChild(span);
+    divContainer.classList.add("container-tasks");
+    divTask.classList.add("task");
+    span.innerText = `${tarefa}`;
+    divTask.appendChild(img);
+    img.src = "assets/remove.svg";
+    img.addEventListener("click", () => {
+      main.removeChild(divContainer);
+    });
+  } else {
+    novaTarefa.style.border = "1px solid red";
+  }
 }
