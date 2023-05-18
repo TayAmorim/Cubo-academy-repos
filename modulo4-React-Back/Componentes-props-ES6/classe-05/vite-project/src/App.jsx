e519d82f79208df108087768a563afb529159f1e
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-18 16:04:01 -0300

    finalizando exercicio 5

--- modulo4-React-Back/Componentes-props-ES6/classe-05/vite-project/src/App.jsx
+++ modulo4-React-Back/Componentes-props-ES6/classe-05/vite-project/src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
 import Card from "./components/card";
+import Cokkies from "./assets/emojione_cookie.png";
+import Alert from "./assets/frame.png";
 
 function App() {
   return (
     <div>
-      <Card />
+      <Card
+        img={Cokkies}
+        text="Nós utilizamos cookies para melhorar nossa UX, analytics e marketing."
+        textButton="Tudo bem!"
+        colorButton="red"
+      />
+      <Card
+        img={Alert}
+        text="Você será deslogado imediatamente!"
+        textButton="Extender login"
+        colorButton="purple"
+      />
     </div>
   );
 }
--- modulo4-React-Back/Componentes-props-ES6/classe-05/vite-project/src/components/Card.jsx
+++ modulo4-React-Back/Componentes-props-ES6/classe-05/vite-project/src/components/Card.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/prop-types */
 import "./card.css";
+import Closed from "../assets/closed.png";
 
-function Card() {
+function Card({ text, textButton, img, colorButton }) {
   return (
-    <div>
-      <h1>oi</h1>
+    <div className="container">
+      <img className="closed-png" src={Closed} alt="" />
+      <img src={img} alt="" />
+      <p className="text-container">{text}</p>
+      <button className={colorButton}>{textButton}</button>
     </div>
   );
 }
