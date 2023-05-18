b0124e90a43933491fa88eab78c2fe495cd821db
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-18 17:08:11 -0300

    adicionando usuarios

--- modulo4-React-Back/Componentes-props-ES6/classe-06/src/App.jsx
+++ modulo4-React-Back/Componentes-props-ES6/classe-06/src/App.jsx
@@ -1,9 +1,48 @@
 import "./App.css";
+import KelvinImg from "../src/assets/kelvin.png";
+import CharlesImg from "../src/assets/charles.png";
+import AnnasImg from "../src/assets/anna.png";
+import MarioImg from "../src/assets/mario.png";
+
+const user = [
+  {
+    id: 1,
+    img: { KelvinImg },
+    name: "Kelvin Costa",
+    social: "@costa",
+    followers: 140,
+    following: 207,
+  },
+  {
+    id: 2,
+    img: { CharlesImg },
+    name: "Charles Santos",
+    social: "@charles.santos",
+    followers: 302,
+    following: 419,
+  },
+  {
+    id: 3,
+    img: { AnnasImg },
+    name: "Anna Bia",
+    social: "@anab",
+    followers: 842,
+    following: 150,
+  },
+  {
+    id: 4,
+    img: { Mario },
+    name: "Mario Hisashi",
+    social: "@hisashi",
+    followers: 28,
+    following: 17,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Teste</h1>
+      <h2>oi</h2>
     </div>
   );
 }
