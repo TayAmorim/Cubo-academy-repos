2bd64e45c9f83d617e6a2aed1ec913333469dec6
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-22 18:51:45 -0300

    exercicio finalizado

--- modulo4-React-Back/Front/use-ref/src/App3.js
+++ modulo4-React-Back/Front/use-ref/src/App3.js
@@ -1,12 +1,36 @@
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
 
 function App3() {
+  const strongGreenRef = useRef(null);
+  const strongRedRef = useRef(null);
+  const strongPinkRef = useRef(null);
 
+  function handleClickGreen() {
+    strongGreenRef.current.style.color = "green";
+    strongGreenRef.current.style.fontSize = "32px";
+  }
+
+  function handleClickPink() {
+    strongPinkRef.current.style.color = "pink";
+    strongPinkRef.current.style.fontSize = "32px";
+  }
+
+  function handleClickRed() {
+    strongRedRef.current.style.color = "red";
+    strongRedRef.current.style.fontSize = "32px";
+  }
   return (
     <div className="App">
-      <strong>Daniel</strong>
-      <strong>João</strong>
-      <strong>Pedro</strong>
+      <strong ref={strongGreenRef} onClick={handleClickGreen}>
+        Daniel
+      </strong>
+      <strong ref={strongPinkRef} onClick={handleClickPink}>
+        João
+      </strong>
+      <strong ref={strongRedRef} onClick={handleClickRed}>
+        Pedro
+      </strong>
     </div>
   );
 }
