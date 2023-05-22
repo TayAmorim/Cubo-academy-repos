dd62a4f47fc534512cb0f581651abc564dfc7ec5
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-22 18:39:29 -0300

    finalizando exercicio 2

--- modulo4-React-Back/Front/use-ref/src/App2.js
+++ modulo4-React-Back/Front/use-ref/src/App2.js
@@ -1,39 +1,36 @@
-import { useRef, useEffect } from 'react';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
 
 function App2() {
   const strongRef = useRef(null);
   const h1Ref = useRef(null);
 
   function handleClick() {
-    if (h1Ref.current.style.backgroundColor === 'black') {
-      h1Ref.current.style.backgroundColor = 'white';
-      h1Ref.current.style.color = 'black';
+    if (h1Ref.current.style.backgroundColor === "black") {
+      h1Ref.current.style.backgroundColor = "white";
+      h1Ref.current.style.color = "black";
       return;
     }
 
-    h1Ref.current.style.backgroundColor = 'black';
-    h1Ref.current.style.color = 'white';
+    h1Ref.current.style.backgroundColor = "black";
+    h1Ref.current.style.color = "white";
   }
 
-  useEffect(() => {
-    strongRef.current.addEventListener("mouseover", () => {
-      h1Ref.current.innerText = "Mouse no strong";
-    });
-  
-    strongRef.current.addEventListener("mouseleave", () => {
-      h1Ref.current.innerText = "Mouse fora do strong";
-    });
-  }, [])
- 
+  function handleMouseOver() {
+    h1Ref.current.innerText = "Mouse no strong";
+  }
+
+  function handleMouseLeave() {
+    h1Ref.current.innerText = "Mouse fora do strong";
+  }
 
   return (
     <div className="App">
       <strong
         ref={strongRef}
         onClick={() => handleClick()}
-        // onMouseOver={() => handleMouseOver()}
-        // onMouseLeave={() => handleMouseLeave()}
+        onMouseOver={() => handleMouseOver()}
+        onMouseLeave={() => handleMouseLeave()}
       >
         Passe o mouse aqui...
       </strong>
