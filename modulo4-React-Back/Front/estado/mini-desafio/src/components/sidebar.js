da26ec3f284ef3c81f32b6b0cdcc7b27dff731b5
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-27 09:18:36 -0300

    virar carta iguais

--- modulo4-React-Back/Front/estado/mini-desafio/src/components/sidebar.js
+++ modulo4-React-Back/Front/estado/mini-desafio/src/components/sidebar.js
@@ -2,13 +2,16 @@ import "./style.css";
 import React from "react";
 import Logo from "../assets/Logo.png";
 
-function Sidebar() {
+function Sidebar(cards) {
+  function handleClickButton() {
+    console.log(cards);
+  }
   return (
     <div className="sidebar">
       <div className="sidebar-img">
         <img src={Logo} alt="" />
       </div>
-      <button>RESET</button>
+      <button onClick={handleClickButton}>RESET</button>
     </div>
   );
 }
--- modulo4-React-Back/Front/estado/mini-desafio/src/pages/Main/index.js
+++ modulo4-React-Back/Front/estado/mini-desafio/src/pages/Main/index.js
@@ -1,18 +1,34 @@
-/* eslint-disable jsx-a11y/alt-text */
 import "./style.css";
 import CARDS from "../../cards";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardBack from "../../assets/card-back.png";
 import Sidebar from "../../components/sidebar";
 
 function Main() {
   const [cards, setCards] = useState(CARDS);
+  const cardsSelected = cards.filter((card) => card.turned);
+  const cardsClear = cards.filter((card) => !card.turned);
+
+  useEffect(() => {
+    if (cardsSelected.length === 2) {
+      setTimeout(() => {
+        if (cardsSelected[0].slug !== cardsSelected[1].slug) {
+          setCards(cards.map((item) => ({ ...item, turned: false })));
+        } else {
+          setCards(cardsClear);
+        }
+      }, 1000);
+    }
+  }, [cards, cardsSelected, cardsClear]);
 
   function handleClick(card) {
+    if (cardsSelected.length >= 2) {
+      return;
+    }
+
     setCards(
       cards.map((item) => {
         if (item.id !== card.id) return item;
-
         return { ...card, turned: true };
       })
     );
@@ -20,13 +36,14 @@ function Main() {
 
   return (
     <div className="container">
-      <Sidebar key="one" />
+      <Sidebar element={cards} />
       <div className="cards-container">
         {cards.map((card) => (
           <div key={card.id} className="cards">
             <img
               onClick={() => handleClick(card)}
               src={card.turned ? card.image : CardBack}
+              alt="opa"
             />
           </div>
         ))}
