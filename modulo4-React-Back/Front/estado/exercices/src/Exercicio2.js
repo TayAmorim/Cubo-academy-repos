8a1a64e9481845a409a25f55bec94d38e8bb0464
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-27 09:59:29 -0300

    exercicio respondido

--- modulo4-React-Back/Front/estado/exercices/src/Exercicio2.js
+++ modulo4-React-Back/Front/estado/exercices/src/Exercicio2.js
@@ -1,8 +1,22 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function Exercicio2() { 
+function Exercicio2() {
+  const listaFrutas = ["uva", "maça", "banana", "melão", "mamão"];
+  const [frutas, setFrutas] = useState(listaFrutas);
+  function addfruta() {
+    setFrutas([...frutas, "goiaba"]);
+  }
+
   return (
-    <h1>Exercicio2</h1>
+    <diiv>
+      <ul>
+        {frutas.map((fruta) => (
+          <li key={fruta}>{fruta}</li>
+        ))}
+      </ul>
+      <button onClick={addfruta}>Adicionar</button>
+    </diiv>
   );
 }
 
