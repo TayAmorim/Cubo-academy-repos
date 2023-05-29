1b329b4bde8fd3328902dba67993ddbde05a99df
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-29 10:31:32 -0300

    regra de negocio para adicionar convidado realizado

--- modulo4-React-Back/Back/API-rest/exercicios-praticos/02/src/controladores/convidados.js
+++ modulo4-React-Back/Back/API-rest/exercicios-praticos/02/src/controladores/convidados.js
@@ -1,4 +1,4 @@
-const convidados = require("../bancodedados");
+let convidados = require("../bancodedados");
 
 const consultarListaInteiraConvidados = (req, res) => {
   const { nome } = req.query;
@@ -14,4 +14,23 @@ const consultarListaInteiraConvidados = (req, res) => {
   return res.json({ mensagem: "Convidado presente." });
 };
 
-module.exports = { consultarListaInteiraConvidados };
+const adicionarConvidado = (req, res) => {
+  const { nome } = req.body;
+  if (!nome) {
+    return res.status(400).json({ mensagem: "É necessário informar um nome" });
+  }
+
+  const convidadoPresente = convidados.find((convidado) => convidado === nome);
+  if (convidadoPresente) {
+    return res.status(404).json({
+      mensagem:
+        "O nome do convidado a ser adicionado já existe na lista. Caso queria adicionar outro convidado de mesmo nome, favor fornecer o sobrenome também.",
+    });
+  }
+  if (!convidadoPresente) {
+    convidados.push(nome);
+    return res.json({ mensagem: "Convidado adicionado." });
+  }
+};
+
+module.exports = { consultarListaInteiraConvidados, adicionarConvidado };
--- modulo4-React-Back/Back/API-rest/exercicios-praticos/02/src/rotas.js
+++ modulo4-React-Back/Back/API-rest/exercicios-praticos/02/src/rotas.js
@@ -4,5 +4,6 @@ const convidados = require("./controladores/convidados");
 const rotas = express();
 
 rotas.get("/convidados", convidados.consultarListaInteiraConvidados);
+rotas.post("/convidados", convidados.adicionarConvidado);
 
 module.exports = rotas;
