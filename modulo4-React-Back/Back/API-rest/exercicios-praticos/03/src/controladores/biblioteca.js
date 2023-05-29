71899edceec705c62283e6ed73f54ce3c1f1223a
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-29 14:17:29 -0300

    regra de negócio de consultar coleção e livro concluido

--- modulo4-React-Back/Back/API-rest/exercicios-praticos/03/src/controladores/biblioteca.js
+++ modulo4-React-Back/Back/API-rest/exercicios-praticos/03/src/controladores/biblioteca.js
@@ -1,1 +1,24 @@
-const { idLivros, livros } = require("../bancodedados");
+let { idLivros, livros } = require("../bancodedados");
+
+const consultarColecao = (req, res) => {
+  res.json(livros);
+};
+
+const consultarLivroId = (req, res) => {
+  const { id } = req.params;
+  const livroPesquisado = livros.find((livro) => livro.id === Number(id));
+  if (!Number(id)) {
+    return res.status(400).json({
+      mensagem: "O valor do parâmetro ID da URL não é um número válido.",
+    });
+  }
+
+  if (!livroPesquisado) {
+    return res.status(404).json({
+      mensagem: "Não existe livro para o ID informado.",
+    });
+  }
+  return res.json(livroPesquisado);
+};
+
+module.exports = { consultarColecao, consultarLivroId };
--- modulo4-React-Back/Back/API-rest/exercicios-praticos/03/src/rotas.js
+++ modulo4-React-Back/Back/API-rest/exercicios-praticos/03/src/rotas.js
@@ -1,5 +1,9 @@
 const express = require("express");
+let biblioteca = require("./controladores/biblioteca");
 
 const rotas = express();
+
+rotas.get("/livros", biblioteca.consultarColecao);
+rotas.get("/livros/:id", biblioteca.consultarLivroId);
 
 module.exports = rotas;
