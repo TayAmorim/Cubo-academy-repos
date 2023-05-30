63e6c0d1d447f7f65ba531a68129fe698f6e8c99
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-05-30 11:43:13 -0300

    style do app concluido

--- modulo4-React-Back/Front/mini-desafio/src/App.js
+++ modulo4-React-Back/Front/mini-desafio/src/App.js
@@ -1,12 +1,17 @@
-import { useState } from "react";
 import "./App.css";
 import Logo from "./assets/logo.svg";
 import Profile from "./assets/profile.jpg";
-import MUSICS from "./musics";
+import Musics from "./musics";
 import CardMusic from "./components/card";
+import { useRef } from "react";
+import Stop from "./assets/stop.svg";
+import Previous from "./assets/previous.svg";
+import Pause from "./assets/pause.svg";
+import Next from "./assets/next.svg";
 
 function App() {
-  const [musics, setMusics] = useState(MUSICS);
+  const audioRef = useRef(null);
+
   return (
     <div className="container">
       <header className="header-container">
@@ -16,11 +21,11 @@ function App() {
           <p>Bem Vinda, Tayanna.</p>
         </div>
       </header>
-      <main>
+      <div>
         <section className="playList">
           <h1>The best play list</h1>
           <div className="container-cards">
-            {musics.map((music) => (
+            {Musics.map((music) => (
               <CardMusic
                 key={music.id}
                 img={music.cover}
@@ -30,7 +35,33 @@ function App() {
             ))}
           </div>
         </section>
-      </main>
+        <audio ref={audioRef}>
+          <source
+            src="https://storage.googleapis.com/pedagogico/frontend-files/aula-react-referencias-eventos/The%20Von%20Trapp%20Family%20Choir%20-%20Alge.mp3"
+            type="audio/mp3"
+          />
+          seu navegador não suporta HTML5
+        </audio>
+        <section className="control-playlist">
+          <div className="info">
+            <p className="info-music">Violão acústico</p>
+            <p className="info-artist">Robert Mayer</p>
+          </div>
+          <div className="control">
+            <div className="control-btn">
+              <img src={Stop} alt="" />
+              <img src={Previous} alt="" />
+              <img src={Pause} alt="" />
+              <img src={Next} alt="" />
+            </div>
+            <div className="control-visual">
+              <span>0</span>
+              <input className="range-music" type="range" />
+              <span>1:36</span>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
