c09a1e5d6640b9911808fe39407ebc294779a145
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-06-03 16:24:50 -0300

    falta colocar value nos outros inputs

--- modulo4-React-Back/Front/formularios/ex2/vite-project/src/pages/main.jsx
+++ modulo4-React-Back/Front/formularios/ex2/vite-project/src/pages/main.jsx
@@ -1,14 +1,33 @@
 import "./main.css";
 import OpenEye from "../assets/open-eye.svg";
 import ImgBackground from "../assets/background.jpg";
+import { useState } from "react";
 
 function Main() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  function inputChange({ target }) {
+    const update = target.name;
+    const value = target.value;
+    setForm({ ...form, [update]: value });
+  }
+
   return (
     <div className="wrapper-center">
       <div className="form-wrapper">
         <h1>Cadastre-se</h1>
         <form className="form-login">
-          <input type="text" name="name" placeholder="Nome" />
+          <input
+            onChange={inputChange}
+            value={form.name}
+            type="text"
+            name="name"
+            placeholder="Nome"
+          />
           <input type="email" name="email" placeholder="E-mail" />
           <div className="wrapper-password">
             <input type="password" name="password" placeholder="Senha" />
