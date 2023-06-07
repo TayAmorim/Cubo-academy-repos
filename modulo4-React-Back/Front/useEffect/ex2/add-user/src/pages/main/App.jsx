9d3bfbd346ab842810f37497606af21f240b4b8d
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-06-07 17:38:32 -0300

    estilo mensage positiva feito

--- modulo4-React-Back/Front/useEffect/ex2/add-user/src/pages/main/App.jsx
+++ modulo4-React-Back/Front/useEffect/ex2/add-user/src/pages/main/App.jsx
@@ -8,24 +8,34 @@ function App() {
   function handleChangeInput({ target }) {
     setInputValue(target.value);
   }
+
+  function handleClickUser() {
+    setUsuarios([...usuarios, inputValue]);
+    setInputValue("");
+  }
   return (
-    <div className="wrapper-main">
-      <div className="sidebar left">
-        <div className="wrapper-control">
-          <input type="text" value={inputValue} onChange={handleChangeInput} />
-          <button onClick={() => setUsuarios([...usuarios, inputValue])}>
-            Adicionar
-          </button>
+    <>
+      <div className="wrapper-main">
+        <div className="sidebar left">
+          <div className="wrapper-control">
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleChangeInput}
+            />
+            <button onClick={handleClickUser}>Adicionar</button>
+          </div>
+        </div>
+        <div className="sidebar rigth">
+          <h1>Usuários</h1>
+          <div className="wrapper-user">
+            {usuarios &&
+              usuarios.map((usuario) => <p key={usuario}>{usuario}</p>)}
+          </div>
         </div>
       </div>
-      <div className="sidebar rigth">
-        <h1>Usuários</h1>
-        <div className="wrapper-user">
-          {usuarios &&
-            usuarios.map((usuario) => <p key={usuario}>{usuario}</p>)}
-        </div>
-      </div>
-    </div>
+      <p className="message">Novo Usuário adicionado</p>
+    </>
   );
 }
 
