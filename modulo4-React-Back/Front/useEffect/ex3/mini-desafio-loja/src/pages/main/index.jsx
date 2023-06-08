ba8f205a6828d8426b38c6e20840d35f337f3a06
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-06-08 16:04:37 -0300

    parte de estilo e html do projeto finalizado

--- modulo4-React-Back/Front/useEffect/ex3/mini-desafio-loja/src/pages/main/index.jsx
+++ modulo4-React-Back/Front/useEffect/ex3/mini-desafio-loja/src/pages/main/index.jsx
@@ -2,6 +2,8 @@ import "./style.css";
 import Logo from "../../assets/logo.svg";
 import SHOES from "../../data";
 import CardShoes from "../../components/Card-shoes";
+import FacebookIcon from "../../assets/facebook.svg";
+import InstagramIcon from "../../assets/instagram.svg";
 
 function Main() {
   return (
@@ -18,6 +20,26 @@ function Main() {
           <CardShoes {...shoe} key={shoe.id} />
         ))}
       </main>
+      <footer>
+        <div className="wrapper-footer">
+          <div className="address">
+            <div className="text-address">
+              <h3 className="title-address">Endereço: </h3>
+              <p>Rua Cubos , 10</p>
+              <p>Jardim Academy</p>
+              <p>Salvador - Bahia - CEP 41820-021</p>
+            </div>
+            <div className="social-address">
+              <img src={FacebookIcon} alt="Icone do Facebook" />
+              <img src={InstagramIcon} alt="Icone do instagram" />
+            </div>
+          </div>
+          <div className="line"></div>
+          <div className="logo">
+            <img src={Logo} alt="Logo da Cubos" />
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
