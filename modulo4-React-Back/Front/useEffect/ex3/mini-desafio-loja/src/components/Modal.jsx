2d56a3193e604435c3562c5b16ccaaba66c69772
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-06-10 06:59:47 -0300

    style[modal]: foi criado a parte tanto do html como do stylo do modal

--- modulo4-React-Back/Front/useEffect/ex3/mini-desafio-loja/src/components/Modal.jsx
+++ modulo4-React-Back/Front/useEffect/ex3/mini-desafio-loja/src/components/Modal.jsx
@@ -8,18 +8,25 @@ function Modal() {
         <div>
           <img src="" alt="img sapato" />
         </div>
-        <h2>Nome do sapato</h2>
-        <p>Descrição do sapato</p>
+        <h2 className="name-shoes">Nome do sapato</h2>
+        <p className="description-shoes">
+          Sofisticado e cheio de elegância, este sapato estilo Derby deixará o
+          seu look impecável. Feito de couro, conta com design moderno, solado
+          flexível e palmilha extra macia. Com muito bom gosto e charme, é o
+          tipo de sapato masculino que se adapta a qualquer ocasião, desde as
+          formais as mais casuais. Fica incrível se combinado com camisa social
+          manga longa e calça de linho.{" "}
+        </p>
         <div className="wrapper-purchase">
-          <button>Comprar</button>
+          <button className="btn-buy">COMPRAR</button>
           <div className="product-value">
             <div className="value">
-              <p>R$ 329,90</p>
-              <p>R$ 229,90</p>
+              <p className="oldPrice">R$ 329,90</p>
+              <p className="currentPrice">R$ 229,90</p>
             </div>
             <div className="installments">
-              <p>6x R$ 38,31</p>
-              <p>Sem juros</p>
+              <p className="installment">6x R$ 38,31</p>
+              <p className="fees">Sem juros</p>
             </div>
           </div>
         </div>
