a1d6bd130d3342e78a499ac1db41f8e381837da9
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-08-01 11:17:30 -0300

    responsivo tabela

--- Modulo5-ReactII-BancoDeDados/front/responsividade/exercicio1/src/components/Table/index.js
+++ Modulo5-ReactII-BancoDeDados/front/responsividade/exercicio1/src/components/Table/index.js
@@ -1,16 +1,17 @@
-import { useEffect, useState } from 'react';
-import DeleteIcon from '../../assets/delete-icon.svg';
-import EditIcon from '../../assets/edit-icon.svg';
-import useContacts from '../../hooks/useContacts';
-import useGlobal from '../../hooks/useGlobal';
-import ModalConfirmDelete from '../ModalConfirmDelete';
-import './styles.css';
+import { useEffect, useState } from "react";
+import DeleteIcon from "../../assets/delete-icon.svg";
+import EditIcon from "../../assets/edit-icon.svg";
+import useContacts from "../../hooks/useContacts";
+import useGlobal from "../../hooks/useGlobal";
+import ModalConfirmDelete from "../ModalConfirmDelete";
+import "./styles.css";
 
 function Table() {
   const { allContacts } = useContacts();
 
   const [open, setOpen] = useState(false);
-  const { setOpenAddEditModal, setCurrentContact, currentContact } = useGlobal();
+  const { setOpenAddEditModal, setCurrentContact, currentContact } =
+    useGlobal();
 
   useEffect(() => {
     if (!open) {
@@ -19,12 +20,12 @@ function Table() {
   }, [open, setCurrentContact]);
 
   function handleEditContact(contact) {
-    setCurrentContact(contact)
+    setCurrentContact(contact);
     setOpenAddEditModal(true);
   }
 
   function handleDeleteContact(contact) {
-    setCurrentContact(contact)
+    setCurrentContact(contact);
     setOpen(true);
   }
 
@@ -32,14 +33,14 @@ function Table() {
     <div className="container-table">
       <div className="header-table">
         <strong>Nome</strong>
-        <strong>Email</strong>
+        <strong className="email">Email</strong>
         <strong>Telefone</strong>
       </div>
       <div className="body-table">
-        {allContacts.map(item => (
+        {allContacts.map((item) => (
           <div className="line-table" key={item.id}>
             <span>{item.nome}</span>
-            <span>{item.email}</span>
+            <span className="email">{item.email}</span>
             <span>{item.telefone}</span>
             <div className="action-buttons">
               <img
@@ -54,15 +55,14 @@ function Table() {
               />
             </div>
           </div>
-        ))
-        }
-      </div >
+        ))}
+      </div>
       <ModalConfirmDelete
         open={open}
         setOpen={setOpen}
         itemToDelete={currentContact}
       />
-    </div >
+    </div>
   );
 }
 
--- Modulo5-ReactII-BancoDeDados/front/responsividade/exercicio1/src/routes.js
+++ Modulo5-ReactII-BancoDeDados/front/responsividade/exercicio1/src/routes.js
@@ -1,21 +1,21 @@
 import {
-  BrowserRouter as Router, Redirect, Route,
-  Switch
-} from 'react-router-dom';
-import { GlobalProvider } from './context/GlobalContext';
-import { ContactsProvider } from './context/ContactsContext';
-import useGlobal from './hooks/useGlobal';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { GlobalProvider } from "./context/GlobalContext";
+import { ContactsProvider } from "./context/ContactsContext";
+import useGlobal from "./hooks/useGlobal";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function ProtectedRoutes(props) {
   const { token } = useGlobal();
 
   return (
-    <Route
-      render={() => (token ? props.children : <Redirect to="/" />)}
-    />
+    <Route render={() => (token ? props.children : <Redirect to="/" />)} />
   );
 }
 
@@ -24,11 +24,11 @@ function Routes() {
     <Router>
       <Switch>
         <GlobalProvider>
-          <Route path={['/', '/sign-in']} exact component={SignIn} />
-          <Route path='/sign-up' exact component={SignUp} />
+          <Route path={["/", "/sign-in"]} exact component={SignIn} />
+          <Route path="/sign-up" exact component={SignUp} />
           <ProtectedRoutes>
             <ContactsProvider>
-              <Route path='/home' exact component={Home} />
+              <Route path="/home" exact component={Home} />
             </ContactsProvider>
           </ProtectedRoutes>
         </GlobalProvider>
