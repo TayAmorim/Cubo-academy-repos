73f0e4f0c58843cb6e49bb45d9797975c46cde94
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-08-02 10:58:18 -0300

    atenticação do token para outras rotas concluido

---
+++ Modulo5-ReactII-BancoDeDados/back/autenticacao-criptografia/src/intermediarios/autenticacao.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const pool = require("../conexao");
+const senhaJwt = require("../senhaJwt");
+
+const verificarUsuariosLogado = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ message: "Não autorizado" });
+  }
+  const token = authorization.split(" ")[1];
+
+  try {
+    const { id } = jwt.verify(token, senhaJwt);
+    const { rows, rowCount } = await pool.query(
+      "select * from usuarios where id = $1",
+      [id]
+    );
+    req.usuario = rows[0];
+    next();
+  } catch (error) {
+    res.status(404).json({ message: `${error?.message}` });
+  }
+};
+
+module.exports = verificarUsuariosLogado;
--- Modulo5-ReactII-BancoDeDados/back/autenticacao-criptografia/src/rotas.js
+++ Modulo5-ReactII-BancoDeDados/back/autenticacao-criptografia/src/rotas.js
@@ -4,10 +4,17 @@ const {
   validarCamposVazios,
   validarCamposLogin,
 } = require("./intermediarios/validarCamposVazios");
+const verificarUsuariosLogado = require("./intermediarios/autenticacao");
 
 const rotas = express();
 
 rotas.post("/usuario", validarCamposVazios, cadastrarUsuario);
 rotas.post("/login", validarCamposLogin, loginUsuario);
+
+rotas.use(verificarUsuariosLogado);
+
+rotas.get("/", (req, res) => {
+  res.json("teste");
+});
 
 module.exports = rotas;
