85499c550df300dc5722cb5e86a45f35f72f9208
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-08-08 15:34:21 -0300

    eexercicio finalizado

---
+++ Modulo5-ReactII-BancoDeDados/front/integral-linter/cubos-desafio2/src/components/PriceButton/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+function PriceButton({ onClickHandler, text, price, type }) {
+  return (
+    <button className={"price-button " + type} onClick={onClickHandler}>
+      {text} 
+      <span className="price">R$ {price.toString().replace('.', ',')}</span>
+    </button>
+  );
+}
+
+export default PriceButton;
